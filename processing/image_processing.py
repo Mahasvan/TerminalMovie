4d33e1b6cfd39032e756189c0c99d0bb1850514b
Mahasvan <mahasvan@gmail.com>
2025-03-03 08:11:07 +0530

    improve image processing, add features

--- processing/image_processing.py
+++ processing/image_processing.py
@@ -1,39 +1,43 @@
 import PIL.Image
+import cv2
 import numpy
 from PIL import Image, ImageFilter
 
 import numpy as np
 
+from collections import Counter
+
 class ImageLoader:
     def __init__(self, path: str):
         self.path = path
-        self.numpy = np.array(PIL.Image.open(path))
+        self.pil_image_color = PIL.Image.open(path)
+        self.numpy_color = numpy.array(self.pil_image_color)
 
-    def load_numpy(self):
-        return self.numpy
+        self.pil_image = self.pil_image_color.convert('L')
+        self.numpy = np.array(self.pil_image)
 
 def get_char(angle):
-    if -180 <= angle <= -157.5:
+    if -181 <= angle <= -157.5:
         return "－"
     if -157.5 <= angle <= -112.5:
-        return "＼"
+        return "／"
     if -112.5 <= angle <= -67.5:
         return "｜"
-    if -67.5 <= angle <= -45:
-        return "／"
-    if -45 <= angle <= -22.5:
-        return "／"
+    if -67.5 <= angle <= -22.5:
+        return "＼"
     if -22.5 <= angle <= 22.5:
         return "－"
-    if 22.5 <= angle <= 45:
-        return "＼"
-    if 45 <= angle <= 90:
-        return "｜"
-    if 90 <= angle <= 135:
+    if 22.5 <= angle <= 67.5:
         return "／"
-    if 135 <= angle <= 180:
+    if 67.5 <= angle <= 112.5:
+        return "｜"
+    if 112.5 <= angle <= 157.5:
+        return "＼"
+    if 157.5 <= angle <= 181.0:
         return "－"
-    else: return "　"
+
+    else:
+        return "　"
 
 class Circle:
     def __init__(self, image_width, radius):
@@ -67,52 +71,33 @@ class ImageProcessor:
         self.sobel_x_kernel = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
         self.sobel_y_kernel = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
         self.image_path = image_path
+        self.image_loader = ImageLoader(image_path)
+
+        self.contour_threshold = 25
+        self.sobel_kernel_size = 3
 
     def downsample(self, chunk_size=4):
-        image = np.array(Image.open(self.image_path))
+        image = self.image_loader.numpy.copy()
         new = image[::chunk_size, ::chunk_size]
         return PIL.Image.fromarray(new)
 
     def find_edges(self):
         # img_1 = np.array(image.filter(ImageFilter.GaussianBlur(radius=2)))
         # img_2 = np.array(image.filter(ImageFilter.GaussianBlur(radius=1)))
-        image = Image.open(self.image_path)
-        image = image.convert('L')
-        return np.array(image.filter(self.edge_kernel))
+        image = self.image_loader.pil_image
+
+        return np.array(image.filter(ImageFilter.FIND_EDGES))
+        # return np.array(image.filter(self.edge_kernel))
         # return img_2 - img_1
 
+
     def sobel(self):
-        image = Image.open(self.image_path)
-        image = image.convert('L')
-        image = np.array(image).astype("float")
-        x_output = np.zeros_like(image)
-        y_output = np.zeros_like(image)
-        for i in range(1, image.shape[0]-1):
-            for j in range(1, image.shape[1]-1):
-                x_output[i, j] = (image[i-1:i+2, j-1:j+2] * self.sobel_x_kernel).sum()
-                y_output[i, j] = (image[i-1:i+2, j-1:j+2] * self.sobel_y_kernel).sum()
-        return x_output, y_output
+        image = self.image_loader.numpy
 
-    def expand_edge(self, matrix):
-        for i in range(matrix.shape[0]):
-            for j in range(matrix.shape[1]):
-                if matrix[i, j] > 50:
-                    matrix[i, j] = 1
-                else:
-                    matrix[i, j] = 0
+        sobelx = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=self.sobel_kernel_size)
+        sobely = cv2.Sobel(image,cv2.CV_64F,0,1,ksize=self.sobel_kernel_size)
 
-        res = np.zeros_like(matrix)
-        neighbours = [(0,1), (0,-1), (1,0), (1,-1), (1,1), (1,-1), (-1,1), (-1,-1)]
-        for i in range(1, matrix.shape[0]-1):
-            for j in range(1, matrix.shape[1]-1):
-                if matrix[i, j] == 1:
-                    res[i, j] = 0
-                # check if neighbours are 1
-                for dx, dy in neighbours:
-                    if matrix[i+dx, j+dy] == 1:
-                        res[i, j] = 255
-                        break
-        return res
+        return sobelx, sobely
 
 
     def edge_highlighting(self, downsample: int = 4):
@@ -123,29 +108,25 @@ class ImageProcessor:
         angles = np.arctan2(result_x, result_y)
 
         edges = edges[::downsample, ::downsample]
-        # edges = self.expand_edge(edges)
         angles = angles[::downsample, ::downsample]
 
         scaled_angles = angles * 180 / np.pi
         chars = np.zeros_like(scaled_angles).astype(str)
         for i in range(scaled_angles.shape[0]):
             for j in range(scaled_angles.shape[1]):
-                if edges[i, j] >= 50:
+                if edges[i, j] >= self.contour_threshold:
                     chars[i, j] = get_char(scaled_angles[i, j])
                 else:
                     chars[i, j] = "　"
         return chars
 
     def intensity_char_mapping(self, downsample: int = 4):
-        chars = "　．，：～０＆％＠＃"
+        chars = "　．，：～０＆％＃＠"
         limits = [255/len(chars) * x for x in range(1,11)]
         # print(limits)
         # ten values, steps of 25.5
 
-        image = Image.open(self.image_path)
-        image = image.convert('L')
-        image = np.array(image).astype("float")
-        image = image[::downsample, ::downsample]
+        image = self.image_loader.numpy[::downsample, ::downsample]
 
         res = numpy.zeros_like(image).astype(str)
         for i in range(image.shape[0]):
@@ -165,3 +146,16 @@ class ImageProcessor:
                     intensities[i, j] = contours[i,j]
 
         return intensities
+
+    def pooling(self, downsample: int = 4):
+        image = self.image_loader.numpy_color
+        rows, cols = image.shape[0]//downsample, image.shape[1]//downsample
+        res = np.zeros((rows, cols, image.shape[2]), dtype=self.image_loader.numpy.dtype)
+
+        for i in range(rows):
+            for j in range(cols):
+                pixels = image[i*downsample:i*downsample+downsample, j*downsample:j*downsample+downsample]
+                unique, counts = np.unique(pixels, return_counts=True, axis=1)
+                least_common = unique[np.argmax(counts)][0]
+                res[i, j] = least_common
+        return res
