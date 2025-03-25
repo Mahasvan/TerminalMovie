543bca21eb5666e5bdf259fd9d6a1dc39ae0c94b
Mahasvan <mahasvan@gmail.com>
2025-03-25 08:38:08 +0530

    make a working demo

--- main.py
+++ main.py
@@ -4,28 +4,11 @@ from processing.video_processing import VideoProcessor
 import numpy as np
 
 # file_path = input("Enter the path of the video: ")
-file_path = "rec.mp4"
-file_path = file_path.replace("'", "")
+file_path = "try_trim.mp4"
+output_path = "outs"
 
-processor = VideoProcessor(file_path, 'frames')
-
-fps, width, height, channels, image = processor.get_deets()
-
-print(image[0][0])
-print(image.shape)
-image = image.mean(axis=2).astype(int)
-
-# import matplotlib.pyplot as plt
-# plt.imshow(image, cmap='gray')
-# plt.show()
-
-sobel_x = [[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]
-sobel_y = [[1, 2, 1], [0, 0, 0], [-1, -2, -1]]
-
-def convolve(image, kernel):
-    kernel = np.array(kernel)
-    output = np.zeros_like(image)
-    for i in range(1, image.shape[0]-1):
-        for j in range(1, image.shape[1]-1):
-            output[i, j] = (image[i-1:i+2, j-1:j+2] * kernel).sum()
-    return output
+processor = VideoProcessor(file_path, 'frames', output_path="outs")
+# processor.split_to_frames()
+# processor.get_edges(downsample=2)
+# processor.get_full_frame(downsample=2)
+processor.print(speed=0.5, combined=True)
--- processing/video_processing.py
+++ processing/video_processing.py
@@ -1,14 +1,27 @@
+import json
+import math
+
 import cv2
 import os
+from processing.image_processing import ImageProcessor
+
+import time
 
 class VideoProcessor:
-    def __init__(self, file_path, frame_path) -> None:
+    def __init__(self, file_path, frame_path, output_path) -> None:
         self.path = file_path
         self.frame_path = frame_path
+        self.output_path = output_path
+
+        if not os.path.exists(self.output_path):
+            os.makedirs(self.output_path, exist_ok=True)
+        if not os.path.exists(self.frame_path):
+            os.makedirs(self.frame_path, exist_ok=True)
+
         self.frame_count = 0
-        self.height = 0
-        self.width = 0
-        raise NotImplementedError
+
+        self.fps, self.height,self.width, self.frame_count, self.image = self.get_deets()
+
 
     def split_to_frames(self):
         # Split video to frames
@@ -20,16 +33,68 @@ class VideoProcessor:
             cv2.imwrite(save_path + f"{count}.jpg", image)     # save frame as JPEG file      
             print("Saved to:", save_path + f"{count}.jpg")
             success,image = vidcap.read()
-            self.frame_count += 1
-            count += 1  
+            count += 1
 
     def get_deets(self):
         vidcap = cv2.VideoCapture(self.path)
         fps = vidcap.get(cv2.CAP_PROP_FPS)
+        frames = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
         success, image = vidcap.read()
         height, width, layers = image.shape
 
-        self.height = height
-        self.width = width
+        return fps, height, width, frames, image
 
-        return fps, height, width, layers, image
+    def get_edges(self, downsample=4):
+        frames = os.listdir(self.frame_path)
+        n_digits = int(math.log(self.frame_count, 10)) + 1
+
+        print(len(frames))
+        for frame in frames:
+            number = frame.lstrip("frame").rstrip(".jpg")
+            processor = ImageProcessor(os.path.join(self.frame_path, frame))
+            edges = processor.edge_highlighting(downsample=downsample)
+            with open(os.path.join(self.output_path, f"edges{int(number):0{n_digits}d}.txt"), 'w') as f:
+                for i in range(edges.shape[0]):
+                    f.write("".join(list(edges[i, :])))
+                    f.write("\n")
+
+    def get_full_frame(self, downsample=4):
+        frames = os.listdir(self.frame_path)
+        n_digits = int(math.log(self.frame_count, 10)) + 1
+
+        print(len(frames))
+        for frame in frames:
+            number = frame.lstrip("frame").rstrip(".jpg")
+            processor = ImageProcessor(os.path.join(self.frame_path, frame))
+            edges = processor.asciify(downsample=downsample)
+            with open(os.path.join(self.output_path, f"combined{int(number):0{n_digits}d}.txt"), 'w') as f:
+                for i in range(edges.shape[0]):
+                    f.write("".join(list(edges[i, :])))
+                    f.write("\n")
+
+    def print(self, speed=1, combined=False, edges=False):
+        if combined:
+            files = [file for file in os.listdir(self.output_path) if file.startswith("combined")]
+        elif edges:
+            files = [file for file in os.listdir(self.output_path) if file.startswith("edges")]
+        else:
+            files = [file for file in os.listdir(self.output_path) if file.startswith("intensity")]
+
+        files.sort()
+        fps = self.fps * speed
+        data = []
+        for file in files:
+            with open(os.path.join(self.output_path, file), "r") as f:
+                data.append(f.read())
+
+        history = []
+        for frame in data:
+            os.system("cls") if os.name == "nt" else os.system("clear")
+            start = time.time()
+            print(frame)
+            end = time.time()
+            diff = end - start
+            history.append(diff)
+            time.sleep(max(1/fps-diff, 0))
+        with open(os.path.join(self.output_path, 'history.txt'), 'w') as f:
+            json.dump(history, f, indent=2)
